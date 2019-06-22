bf1da02bde1efe687384d910a91b2750382e00c9
chouxuewen <chouxuewen@163.com>
2019-06-22 17:51:34 +0800

    创建员工类

---
+++ emp/src/main/java/com/baizhi/entity/Emp.java
@@ -0,0 +1,60 @@
+package com.baizhi.entity;
+
+public class Emp {
+    private String id;
+    private String name;
+    private Integer age;
+    private Double salary;
+
+    public Emp() {
+    }
+
+    public Emp(String id, String name, Integer age, Double salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Emp{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                '}';
+    }
+}
